ed75773384ecedf3a2dbc7ea1e18561cd9a3c8e0
Younguk Kim <bluekyu.dev@gmail.com>
2017-07-24 18:07:39 +0900

    Add cmake files

---
+++ cmake/lui-macro.cmake
@@ -0,0 +1,7 @@
+# variable
+set(lui_DEBUG_POSTFIX "-debug" CACHE INTERNAL
+    "Postfix string used for the name of Debug mode in LUI"
+)
+set(lui_RELWITHDEBINFO_POSTFIX "-reldeb" CACHE INTERNAL
+    "Postfix string used for the name of RelWithDebInfo mode in LUI"
+)
